0ff4f5a62b43c354475ab0ccc7fe9f6527a0dac2
Critonik <42637808+Critonik@users.noreply.github.com>
2019-07-12 22:26:12 +0500

    валидация количества комнат

--- js/form.js
+++ js/form.js
@@ -5,17 +5,51 @@
   var arrivalTime = adForm.querySelector('#timein');
   var departureTime = adForm.querySelector('#timeout');
   var priceInput = adForm.querySelector('#price');
+  var roomNumber = adForm.querySelector('#room_number');
+  var guestNumber = adForm.querySelector('#capacity');
+  var optionsGuests = guestNumber.querySelectorAll('option');
 
-  var offerToValue = {
+  var OfferToValue = {
     bungalo: 0,
     flat: 1000,
     house: 5000,
     palace: 10000
   };
 
+  var GuestsByRoom = {
+    1: ['1'],
+    2: ['1', '2'],
+    3: ['1', '2', '3'],
+    100: ['0']
+  };
+
   placeType.addEventListener('change', function (evt) {
     evt.preventDefault();
-    window.util.changePrice(evt.target.value, priceInput, offerToValue);
+    window.util.changePrice(evt.target.value, priceInput, OfferToValue);
+  });
+
+  var onFieldRoomsChange = function (value) {
+    var availableOptions = GuestsByRoom[value];
+    optionsGuests.forEach(function (option) {
+      option.disabled = availableOptions.indexOf(option.value) === -1;
+    });
+  };
+
+  var onFieldGuestsValidity = function (value) {
+    var availableOptions = GuestsByRoom[value];
+    if (availableOptions.indexOf(guestNumber.value) === -1) {
+      guestNumber.setCustomValidity('Укажите другое количество гостей');
+    }
+    guestNumber.addEventListener('change', function () {
+      if (availableOptions.indexOf(guestNumber.value) !== -1) {
+        guestNumber.setCustomValidity('');
+      }
+    });
+  };
+
+  roomNumber.addEventListener('change', function () {
+    onFieldRoomsChange(roomNumber.value);
+    onFieldGuestsValidity(roomNumber.value);
   });
 
   window.util.syncPlace(arrivalTime, departureTime);
